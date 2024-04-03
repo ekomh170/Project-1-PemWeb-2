f0ad7f4416b05aa7e2f0f85711c5920cd1e3799c
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-04-04 02:18:22 +0700

    1.6. Melakukan Penambahan File Untuk Fungsi Hapus Menu Data Dokter & Hapus Menu Data Periksa Pasien Kepada Dokter

---
+++ periksa/delete.php
@@ -0,0 +1,20 @@
+<?php
+// Memanggil file koneksi database
+require '../dbkoneksi.php';
+
+// Memeriksa apakah parameter id telah diterima dari URL
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Query untuk menghapus data periksa berdasarkan id
+    $sql = "DELETE FROM periksa WHERE id = ?";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([$id]);
+
+    // Redirect ke halaman index.php setelah proses penghapusan selesai
+    header("Location: index.php");
+    exit();
+} else {
+    echo "Parameter ID tidak ditemukan.";
+    exit;
+}
