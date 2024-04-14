9cc35e629ee17dbc5505ed4af003e6ebeec54cc9
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-04-14 15:50:44 +0700

    1.16. Menambahkan Tambah Data & Edit Data Pada Menu Kelurahan

---
+++ kelurahan/add.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+
+require '../dbkoneksi.php';
+if (isset($_POST["submit"])) {
+    $_nama = $_POST['nama'];
+    $data = [$_nama];
+    $sql = "INSERT INTO kelurahan (nama) VALUES (?)";
+    $stmt = $dbh->prepare($sql);
+    if ($stmt->execute($data)) {
+        // Jika eksekusi query berhasil
+        echo "<script>alert('Data berhasil ditambahkan.'); window.location.href = 'index.php';</script>";
+    } else {
+        // Jika eksekusi query gagal
+        echo "<script>alert('Gagal menambahkan data. Silakan coba lagi.');</script>";
+    }
+}
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Menu Tambah Data - Form Kelurahan</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Form Kelurahan</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h2 class="text-center">Form Kelurahan</h2>
+                            <form action="add.php" method="POST">
+                                <div class="form-group row">
+                                    <label for="nama" class="col-4 col-form-label">Nama</label>
+                                    <div class="col-8">
+                                        <input id="nama" name="nama" type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-4 col-8">
+                                        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            Footer
+                        </div>
+                        <!-- /.card-footer-->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
---
+++ kelurahan/edit.php
@@ -0,0 +1,99 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+
+require '../dbkoneksi.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    // Query untuk mengambil data Kelurahan berdasarkan id
+    $sql = "SELECT * FROM kelurahan WHERE id = ?";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([$id]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+if (isset($_POST['submit'])) {
+    $_nama = $_POST['nama'];
+    $data = [$_nama, $id];
+    // Query SQL untuk update data Kelurahan berdasarkan id
+    $sql = "UPDATE kelurahan SET nama = ? WHERE id = ?";
+    $stmt = $dbh->prepare($sql);
+    if ($stmt->execute($data)) {
+        // Jika eksekusi query berhasil
+        echo "<script>alert('Data berhasil diupdate.'); window.location.href = 'index.php';</script>";
+    } else {
+        // Jika eksekusi query gagal
+        echo "<script>alert('Gagal memperbarui data. Silakan coba lagi.');</script>";
+    }
+}
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Menu Edit Data - Form Kelurahan</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Form Kelurahan</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h2 class="text-center">Form Kelurahan</h2>
+                            <form action="edit.php?id=<?= $row['id'] ?>" method="POST">
+                                <div class="form-group row">
+                                    <label for="nama" class="col-4 col-form-label">Nama</label>
+                                    <div class="col-8">
+                                        <input id="nama" name="nama" type="text" class="form-control" value="<?= $row['nama'] ?>">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-4 col-8">
+                                        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            Project 1 - Aplikasi CRUD Sederhana Puskesmas
+                        </div>
+                        <!-- /.card-footer-->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
